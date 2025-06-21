ace9ab00efa7239b9ee2c058677e1174f526723b
Lineker Silva <linekerpablo@gmail.com>
2025-06-21 19:14:58 -0300

    Local

--- app/config/local.php
+++ app/config/local.php
@@ -1,10 +1,10 @@
 <?php
 $parameters = array(
     'db_driver' => 'pdo_mysql',
-    'db_host' => 'mysql.railway.internal',           // ou IP do seu banco
-    'db_port' => '3306',
-    'db_name' => 'railway',
-    'db_user' => 'root',
-    'db_password' => 'njubucDwRjXMpwpCHzoNFWYuPGQfGMsX',
+    'db_host' => getenv('MAUTIC_DB_HOST'),
+    'db_port' => getenv('MAUTIC_DB_PORT'),
+    'db_name' => getenv('MAUTIC_DB_NAME'),
+    'db_user' => getenv('MAUTIC_DB_USER'),
+    'db_password' => getenv('MAUTIC_DB_PASSWORD'),
     'installed' => true,
 );
