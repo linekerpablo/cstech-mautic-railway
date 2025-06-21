744eaaaf17a1f7da0be8223cc1af1a9bf4e46265
Lineker Silva <linekerpablo@gmail.com>
2025-06-21 19:10:11 -0300

    Local.ph

---
+++ app/config/local.php
@@ -0,0 +1,10 @@
+<?php
+$parameters = array(
+    'db_driver' => 'pdo_mysql',
+    'db_host' => 'mysql.railway.internal',           // ou IP do seu banco
+    'db_port' => '3306',
+    'db_name' => 'railway',
+    'db_user' => 'root',
+    'db_password' => 'njubucDwRjXMpwpCHzoNFWYuPGQfGMsX',
+    'installed' => true,
+);
