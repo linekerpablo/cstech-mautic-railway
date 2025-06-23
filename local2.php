89ce585f23b183f24bd657fb0c6f6543432679ee
Lineker Silva <linekerpablo@gmail.com>
2025-06-23 15:40:10 -0300

    Local 2

---
+++ local2.php
@@ -0,0 +1,20 @@
+<?php
+$parameters = array (
+  'db_driver' => 'pdo_mysql',
+  'db_host' => 'db',
+  'db_port' => '3306',
+  'db_name' => 'mauticdb',
+  'db_user' => 'mautic',
+  'db_password' => 'mautic',
+  'site_url' => 'https://mautic.carvalhosilvatech.com.br',
+  'trusted_proxies' => 
+  array (
+    0 => '0.0.0.0/0',
+  ),
+  'install_source' => 'Docker',
+  'installed' => true,
+  'secret_key' => 'a984e76e-93e7-43e9-b4aa-6781d6f6df14',
+  'default_timezone' => 'UTC',
+  'admin_email' => 'admin@localhost',
+  'admin_password' => 'admin',
+);
