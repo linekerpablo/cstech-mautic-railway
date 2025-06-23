0212e460cc8f3757018104ee4624ebab4c30996d
Lineker Silva <linekerpablo@gmail.com>
2025-06-23 10:07:16 -0300

    Local.php

--- local.php
+++ local.php
@@ -2,11 +2,13 @@
 $parameters = array(
     'db_driver' => 'pdo_mysql',
     'db_host' => getenv('MAUTIC_DB_HOST'),
-    'db_port' => getenv('MAUTIC_DB_PORT'),
+    'db_port' => getenv('MAUTIC_DB_PORT') ?: '3306',
     'db_name' => getenv('MAUTIC_DB_NAME'),
     'db_user' => getenv('MAUTIC_DB_USER'),
     'db_password' => getenv('MAUTIC_DB_PASSWORD'),
-    'installed' => true,
     'site_url' => getenv('MAUTIC_URL'),
-    'secret_key' => 'b84c91f2-1d61-4de4-9a75-75b4d18bd2d8'
+    'trusted_proxies' => getenv('MAUTIC_TRUSTED_PROXIES'),
+    'install_source' => 'Docker',
+    'installed' => true,
+    'secret_key' => 'a984e76e-93e7-43e9-b4aa-6781d6f6df14'
 );
