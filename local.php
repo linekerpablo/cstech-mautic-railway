dc67c6c958962bc1e9337a92dd647100b33282fa
Lineker Silva <linekerpablo@gmail.com>
2025-06-23 12:27:56 -0300

    Local.php

--- local.php
+++ local.php
@@ -1,13 +1,13 @@
 <?php
 $parameters = array(
     'db_driver' => 'pdo_mysql',
-    'db_host' => getenv('MAUTIC_DB_HOST'),
-    'db_port' => getenv('MAUTIC_DB_PORT') ?: '3306',
-    'db_name' => getenv('MAUTIC_DB_NAME'),
-    'db_user' => getenv('MAUTIC_DB_USER'),
-    'db_password' => getenv('MAUTIC_DB_PASSWORD'),
-    'site_url' => getenv('MAUTIC_URL'),
-    'trusted_proxies' => getenv('MAUTIC_TRUSTED_PROXIES'),
+    'db_host' => 'mysql.railway.internal',
+    'db_port' => '3306',
+    'db_name' => 'railway',
+    'db_user' => 'root',
+    'db_password' => 'fvJdZNnmtUuiRzcSdfujydMVRzJjuoQl',
+    'site_url' => 'https://mautic.carvalhosilvatech.com.br',
+    'trusted_proxies' => '["0.0.0.0/0"]',
     'install_source' => 'Docker',
     'installed' => true,
     'secret_key' => 'a984e76e-93e7-43e9-b4aa-6781d6f6df14'
